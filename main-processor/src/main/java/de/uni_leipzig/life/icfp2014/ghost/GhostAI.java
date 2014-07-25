c004ea857380011f93c500d0ddf8e98897350f3b
Mathias Rühle <math2306@hotmail.com>
2014-07-26 00:17:08 +0200

    Add First Version
    Generating simple map works.

---
+++ main-processor/src/main/java/de/uni_leipzig/life/icfp2014/ghost/GhostAI.java
@@ -0,0 +1,5 @@
+package de.uni_leipzig.life.icfp2014.ghost;
+
+public interface GhostAI {
+
+}
---
+++ main-processor/src/main/java/de/uni_leipzig/life/icfp2014/main/CellContent.java
@@ -0,0 +1,42 @@
+package de.uni_leipzig.life.icfp2014.main;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+enum CellContent {
+    WALL(true, true, '#'),
+    SPACE(true, true, ' '),
+    PILL(true, true, '.'),
+    POWER_PILL(true, false, 'o'),
+    FRUIT(true, false, '%'),
+    LAMBDA_MAN(true, false, '/'),
+    GHOST(false, false, '=');
+
+    private final boolean isSafe;
+    private final boolean isFree;
+    private char symbol;
+
+    private CellContent(boolean isSafe, boolean isFree, char symbol) {
+        this.isSafe = isSafe;
+        this.isFree = isFree;
+        this.symbol = symbol;
+        
+    }
+    
+    static CellContent valueOf(int ordinal) {
+        checkArgument(ordinal >= 0 && ordinal < values().length, "Given ordinal %s is not in range [0,%s]",
+                ordinal, values().length - 1);
+        return values()[ordinal];
+    }
+
+    boolean isSafe() {
+        return isSafe;
+    }
+
+    boolean isFree() {
+        return isFree;
+    }
+    
+    public char toSymbol() {
+        return symbol;
+    }
+}
