8857a420784875784f21fae48d8057c2c5c12676
Mathias Rühle <math2306@hotmail.com>
2014-07-26 14:28:37 +0200

    Add Test for Higher Level

--- main-processor/src/test/java/de/uni_leipzig/life/icfp2014/main/MazeMapTest.java
+++ main-processor/src/test/java/de/uni_leipzig/life/icfp2014/main/MazeMapTest.java
@@ -1,19 +1,23 @@
 package de.uni_leipzig.life.icfp2014.main;
 
-import static org.hamcrest.CoreMatchers.containsString;
-
-import static org.junit.Assert.assertThat;
-
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
 
 public class MazeMapTest {
 
     @Test
-    public void testGenerateMap() {
+    public void testGenerateMapLevel1() {
         MazeMap mazeMap = new MazeMap(1);
+        assertThat(mazeMap.toString(), containsString("/"));
+        assertThat(mazeMap.toString(), containsString("="));
+        assertThat(mazeMap.toString(), containsString("."));
+    }
+
+    @Test
+    public void testGenerateMapLevel20() {
+        MazeMap mazeMap = new MazeMap(20);
         assertThat(mazeMap.toString(), containsString("/"));
         assertThat(mazeMap.toString(), containsString("="));
         assertThat(mazeMap.toString(), containsString("."));
